3c35a5c930a0796cd400a95bef7afec616758a57
Andrew Weaver <weevz@uw.edu>
2016-11-02 15:36:25 -0400

    changed reporting

--- audiohasher.sh
+++ audiohasher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#depends on chromaprint
+#depends on chromaprint (available via brew install chromaprint)
 
 INPUT1=$(fpcalc -raw "$1" | tail -n1 | cut -d'=' -f2 | tr , '\n' | sort)
 INPUT2=$(fpcalc -raw "$2" | tail -n1 | cut -d'=' -f2 | tr , '\n' | sort)
@@ -7,8 +7,10 @@ INPUT2=$(fpcalc -raw "$2" | tail -n1 | cut -d'=' -f2 | tr , '\n' | sort)
 
 
 HASH_SHARED=$(comm -12 <(echo "$INPUT1") <(echo "$INPUT2") | wc -l)
-HASH_TOTAL=$(echo "$INPUT1" | wc -l)
-echo "$HASH_TOTAL"
-echo "$HASH_SHARED"
+HASH_TOTAL1=$(echo "$INPUT1" | wc -l)
+HASH_TOTAL2=$(echo "$INPUT1" | wc -l)
 
-echo "File hashes have "$HASH_SHARED" similarities out of "$HASH_TOTAL" checked values."
+echo "$(basename "$1") Number of Hashes: $HASH_TOTAL1"
+echo "$(basename "$2") Number of Hashes: $HASH_TOTAL2"
+echo "Number of Common Hashes: $HASH_SHARED"
+
